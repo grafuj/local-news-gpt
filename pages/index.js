b61d62f684974d558bad6f535e2798f5efa011d7
Graham Fujisawa <sfrrki@gmail.com>
2023-09-12 18:18:32 -0700

    moved lobby to index.js, no progress

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import { useRouter } from "next/router";
 
 import { LocalNewsPage } from "./LocalNewsPage";
 import { Lobby } from "./Lobby";
-import "./index.module.css";
+import styles from "./index.module.css";
+
 import News from "./[news]";
 
 export default function Home() {
@@ -15,22 +16,69 @@ export default function Home() {
   const router = useRouter();
   const { query } = router;
 
-  let pageComponent;
   if (query.page === "News") {
-    pageComponent = <News newsCityInput={cityInput} newsResult={result} />;
-  } else {
-    pageComponent = <Lobby cityInput={cityInput} setCityInput={setCityInput} result={result} setResult={setResult} />;
+    return <News newsCityInput={cityInput} newsResult={result} />;
   }
 
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generateCity", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ city: cityInput }),
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+      console.log("data26:", data);
+      console.log("parsed data27:", responseParser(data.result));
+      setResult(responseParser(data.result));
+
+      setTimeout(() => {
+        // setResult(responseParser(data.result));
+        router.push('/News');
+        console.log("result33:", result);
+      }, 1500);
+
+      // router.push("/News");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+
   return (
-    <div>
+    <>
       <Head>
         <title>Local News GPT</title>
         <link rel="icon" href="/dog.png" />
       </Head>
-      {pageComponent}
-      result at index.js 32
-      {/* {result && result[0].title} */}
-    </div>
+
+      <main className={styles.main}>
+        <img src="/dog.png" className={styles.icon} />
+        <h3>Enter a City</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="city"
+            placeholder="Enter a city"
+            value={cityInput}
+            onChange={(e) => setCityInput(e.target.value)}
+          />
+          {/* <Link href="/News"> */}
+          <button type="submit">Generate</button>
+          {/* </Link> */}
+        </form>
+        {/* <div className={styles.result}>lobby 51{result && result[0].title}</div> */}
+        {/* <div className={styles.result}>{result && result[0].descriptions}</div> */}
+      </main>
+    </>
   );
 }
